2b293bb7a6172041255d566bbba857970d538654
MPenunuri <manuel.penunuri.ram@gmail.com>
2023-09-20 10:20:08 -0600

    Add style-loader and css-loader  dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import menu from './menu.js'
 const body = document.getElementById('body');
 const generalContainer = document.getElementById('content');
 
-generalContainer.textContent = 'hola'
+generalContainer.textContent = 'hola!'
 
 console.log(menu)
